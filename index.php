5cccb5f8539bc6475278d75fc47b3bd67846cb75
ZidCoder <ozidhalawa97@gmail.com>
2020-06-08 23:09:20 +0700

    menambahkan pragraf

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi culpa in mollitia dolores repellendus nostrum, rem non est optio cum consequatur qui totam, distinctio a similique. Ea alias necessitatibus eaque!</p>
 </body>
 </html>
